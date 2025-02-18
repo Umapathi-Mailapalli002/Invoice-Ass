ed0ddae3fb04362f69e8bc39f2497892c4598fda
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-18 15:50:39 +0530

    update axios baseURL to use render.com for API requests

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "https://invoice-ass-server.vercel.app/",
-    //baseURL: "https://invoice-ass-server.onrender.com/api/v1",
+    //baseURL: "https://invoice-ass-server.vercel.app/",
+    baseURL: "https://invoice-ass-server.onrender.com/api/v1",
     //baseURL: import.meta.env.VITE_APP_API_BASE_URL,
    // baseURL: "https://86d0-2402-8100-21ff-c00-3da3-465d-608e-1c92.ngrok-free.app/",
     withCredentials: true,
