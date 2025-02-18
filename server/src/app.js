7801f77b3911b453740f6f78437b409f96f7f8b5
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-18 16:07:15 +0530

    configure CORS to allow requests from the client application with credentials

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 // Middleware
 app.use(
-  cors()
+  cors({
+    origin: "https://invoice-ass-client.vercel.app/",
+    credentials: true,
+  })
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
